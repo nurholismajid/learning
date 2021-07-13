392977b896c258821d6f364701e4fe3dbc87506e
Nurholis Majid <nrhlsmjd@gmail.com>
2021-07-13 15:23:50 +0700

    basic js

---
+++ js Basic/index.js
@@ -0,0 +1,42 @@
+let name = "olis";
+console.log(name);
+
+let fristname = "Nurholis", lastname = "Majid";
+console.log(fristname  +  ""  + lastname); 
+
+let angka = 1;
+
+angka = 12 ;
+
+console.log(angka);
+
+
+
+// const jumlah = 10;
+
+// jumlah = 50;
+
+// console.log(jumlah);
+
+// let nama = 'olis';
+// let status = false;
+// let nama = undefined;
+// let nama = null;
+
+let data = {
+    name :"nurholis Majid",
+    age: 20
+}
+
+console.log(data);
+console.log(data.name);
+console.log(data.age);
+
+let makanan =  ["ayam", "mie instan", "susu"];
+
+console.log(makanan[1]);
+
+function nama(nama){
+    return "nama saya adalah :"+nama;
+}
+console.log(nama("Nurholis Majid"))
